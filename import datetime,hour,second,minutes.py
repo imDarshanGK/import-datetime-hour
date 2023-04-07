4ad02a460325e92bf65a114cf319f2f9126919ca
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-04-07 08:16:09 +0530

    import datetime

---
+++ import datetime,hour,second,minutes.py
@@ -0,0 +1,8 @@
+import datetime as d
+print(d.datetime.now())
+print(d.datetime.now().year)
+print(d.datetime.now().month)
+print(d.datetime.now().day)
+print(d.datetime.now().hour)
+print(d.datetime.now().second)
+print(d.datetime.now().minute)
